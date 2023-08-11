dff15ce712b7991149d41a840ec8e0f66b5d2f41
Baldeep Singh <baldeep.singh@pantelwar.com>
2023-08-11 13:20:12 +0530

    fix(utils): added pdf extension handling to mime types

--- src/utils/convert-url-to-file.ts
+++ src/utils/convert-url-to-file.ts
@@ -6,7 +6,8 @@ export const convertUrlToFile = async (url: string) => {
   const res = await fetch(url);
   const data = await res.blob();
   const file = new File([data], filename, {
-    type: extension === 'jpg' ? 'image/jpeg' : 'image/png',
+    type:
+      extension === '.jpg' ? 'image/jpeg' : extension === '.pdf' ? 'application/pdf' : 'image/png',
   });
   Object.assign(file, {
     preview: URL.createObjectURL(file),
